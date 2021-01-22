6b529f7c0bad71643d4512ffaefcb658c505c4eb
Davidyz <hzjlyz@gmail.com>
2021-01-22 05:44:40 +0000

    downloader added.

---
+++ downloader.py
@@ -0,0 +1,19 @@
+import youtube_dl
+
+__ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'aac',
+            'preferredquality': '320'
+            }],
+        'prefer_ffmpeg': True,
+        'outtmpl': '/mnt/wd_blue/Videos/Youtube_dl/%(title)s.%(ext)s'
+        }
+
+__ydl = youtube_dl.YoutubeDL(__ydl_opts)
+
+def Download(url):
+    if isinstance(url, str):
+        url = [url]
+    __ydl.download(url)
