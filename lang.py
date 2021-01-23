c3798a950f140c355538acca632f5a2a9c787fa9
Davidyz <hzjlyz@gmail.com>
2021-01-23 14:58:13 +0000

    Trying to add multi-language support.

---
+++ lang.py
@@ -0,0 +1,19 @@
+zh_CN = {
+        'help': 'help',
+        'finished_download': '<{}> 下载完成',
+        'no_job': '当前下载状态：空闲',
+        'current_job': '正在下载：',
+        'trying': '这是一个{}视频。正在尝试下载...',
+        'unsupported_video': '尚未支持此视频网站。',
+        'welcome': '欢迎使用此Bot.'
+        }
+
+en_UK = {
+        'help':'',
+        'finished_download': '',
+        'no_job': 'No active download.',
+        'current_job': 'Downloading: ',
+        'trying': 'This is a {} video. Trying to download.',
+        'unsupported_video': 'This video source is not supported.',
+        'welcome': 'Welcome to my bot'.
+        } 
